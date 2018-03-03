72c67fa548384e59a224276f510ead0b852d88c8
Capitala <arvind.kanyal@gmail.com>
2018-03-03 22:36:44 +0000

    First Commit

---
+++ src/main/java/com/solution/library/Challenges_the_time_in_words_problem/App.java
@@ -0,0 +1,13 @@
+package com.solution.library.Challenges_the_time_in_words_problem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/solution/library/Challenges_the_time_in_words_problem/AppTest.java
@@ -0,0 +1,16 @@
+package com.solution.library.Challenges_the_time_in_words_problem;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+}
